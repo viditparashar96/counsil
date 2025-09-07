a2ec852928875f38206ec567b2bbf5700c2ac55a
vidit <viditsharma818@gmail.com>
2025-09-07 21:39:31 +0530

    fixed Readme and packagejson

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,9 +14,8 @@ if (typeof window === 'undefined') {
 }
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://chat.vercel.ai'),
-  title: 'Next.js Chatbot Template',
-  description: 'Next.js chatbot template using the AI SDK.',
+  title: 'Career Counseling Chatbot',
+  description: 'Career Counseling Chatbot',
 };
 
 export const viewport = {
